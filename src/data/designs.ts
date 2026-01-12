2336599ae3f55249d7b34294170532c51eec2245
capitaine12 <cn752206@gmail.com>
2026-01-12 18:29:20 +0000

    dd

--- src/data/designs.ts
+++ src/data/designs.ts
@@ -1,6 +1,6 @@
 import type { Design } from '@/types/types';
 
-import img1 from '../assets/images/gallery/img1.png';
+import img1 from '@/assets/images/gallery/img1.png';
 import img2 from '@/assets/images/gallery/img2.png';
 import img3 from '@/assets/images/gallery/img3.png';
 import img4 from '@/assets/images/gallery/img4.png';
