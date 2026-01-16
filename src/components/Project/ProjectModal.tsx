eeda5f8805d80f86bb2fcc28c10e85d0f37f2106
capitaine12 <cn752206@gmail.com>
2026-01-16 18:35:16 +0000

    mobile first project phase 4 drag  indicateur de pagination mobile 2

--- src/components/Project/ProjectModal.tsx
+++ src/components/Project/ProjectModal.tsx
@@ -101,7 +101,7 @@ const ProjectModal = ({ project, onClose }: Props) => {
                 will-change-transform "
               />
 {/* Pagination mobile */}
-<div className="flex lg:hidden justify-center gap-2 mt-3">
+<div className="flex lg:hidden justify-center gap-2">
   {project.images.map((_, i) => {
     const active = i === index;
 
