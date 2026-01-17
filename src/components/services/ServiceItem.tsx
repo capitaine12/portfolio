75af15cb469b2ef19a0683d60f461aa8685e8f7c
capitaine12 <cn752206@gmail.com>
2026-01-17 01:13:45 +0000

    service ajustement resposive

--- src/components/services/ServiceItem.tsx
+++ src/components/services/ServiceItem.tsx
@@ -1,40 +1,57 @@
 import type { Service } from "@/types/types";
 
 interface Props {
-    service: Service;
-    reverse?: boolean;
+  service: Service;
+  reverse?: boolean;
 }
 
 const ServiceItem = ({ service, reverse }: Props) => {
-    return (
-        <div className="grid md:grid-cols-2 gap-8 items-center">
+  return (
+    <div className="grid md:grid-cols-2 gap-8 items-center w-full overflow-hidden">
 
-            {/* Image */}
-            <div className={`w-[400px] h-52 overflow-hidden bg-gray-200
-                    ${reverse ? "md:order-2" : "md:order-1"}
-                    `}>
-                    <img src={service.image}
-                    alt={service.title}
-                    className="w-full h-full object-cover"/>
-            </div>
+      {/* Image */}
+      <div
+        className={`
+          w-full
+          max-w-md
+          h-52
+          overflow-hidden
+          bg-gray-200
+          mx-auto
+          ${reverse ? "md:order-2" : "md:order-1"}
+        `}
+      >
+        <img
+          src={service.image}
+          alt={service.title}
+          className="w-full h-full object-cover"
+        />
+      </div>
 
-            {/* Texte */}
-            <div className={`w-[400px] h-52
-                    ${reverse ? "md:order-1 text-right" : "md:order-2"}
-                    `}>
-                <h2 className="text-2xl font-bold mb-6">
-                    {service.title}
-                </h2>
-                <ul>
-                {service.description.map((text: string, i: number) => (
-                    <li key={i} className="text-sm mb-4">
-                        {text}
-                    </li>
-                ))}
-                </ul>
+      {/* Texte */}
+      <div
+        className={`
+          w-full
+          max-w-md
+          mx-auto
+          ${reverse ? "md:order-1 md:text-right" : "md:order-2"}
+        `}
+      >
+        <h2 className="text-2xl font-bold mb-6">
+          {service.title}
+        </h2>
 
-            </div>
-        </div>
-    );
+        <ul>
+          {service.description.map((text, i) => (
+            <li key={i} className="text-sm mb-4 leading-relaxed">
+              {text}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+    </div>
+  );
 };
+
 export default ServiceItem;
