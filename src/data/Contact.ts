b0b22ae93123774118ca0ed676532e0d90721846
capitaine12 <cn752206@gmail.com>
2026-03-07 17:41:14 +0000

    update link

--- src/data/Contact.ts
+++ src/data/Contact.ts
@@ -1,20 +1,20 @@
 import type { Contact } from "@/types/types";
 
 export const Contacts: Contact[] = [
-  {
+/*   {
     name: 'Instagram',
     icon: "Instagram",
     link: 'https://www.instagram.com',
-  },
+  }, */
   {
     name: 'LinkedIn',
     icon: "LinkedIn",
-    link: 'https://www.linkedin.com',
+    link: 'https://www.linkedin.com/in/cheikh-ndiaye-1a1b80346',
   },
   {
     name: 'WhatsApp',
     icon: "whatsapp",
-    link: 'https://wa.me',
+    link: 'https://wa.me/221780121858',
   },
 
   {
